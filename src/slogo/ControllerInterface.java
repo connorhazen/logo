d544f94f382db525589b3dd5bc7ceee3cad48141
cch57 <christopher.hazen@duke.edu>
2020-02-18 20:44:06 -0500

    initialized all interfaces

---
+++ src/slogo/ControllerInterface.java
@@ -0,0 +1,12 @@
+
+public interface ControllerInterface{
+
+
+  /**
+   * On run button click, calls model.runCommand with input box contents.
+   * If catches error, calls view.printError();
+   */
+  void updateModel(); //private
+
+
+}
