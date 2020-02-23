a06f85e905f28dfb1276943227e78b23a1353896
Sebastian Williams <svw9@duke.edu>
2020-02-23 17:50:18 -0500

    Added basic layout formatting for main window

--- src/slogo/View.java
+++ src/slogo/View.java
@@ -6,6 +6,11 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -17,13 +22,52 @@ public class View implements ViewInterface {
   public View(ControllerInterface cont, Stage primaryStage){
     controller = cont;
     makeScreen(primaryStage);
-    Group root = new Group();
-    Scene scene = new Scene(root, 500, 500);
+    Scene scene = new Scene(createBorderPane(), 500, 500);
     primaryStage.setScene(scene);
     primaryStage.show();
 
     //startAnimation();
 
+  }
+
+  private BorderPane createBorderPane(){
+    BorderPane borderPane = new BorderPane();
+    borderPane.setTop(createTopHBox());
+    borderPane.setRight(createRightVBox());
+    borderPane.setBottom(createBottomHBox());
+    borderPane.setStyle("-fx-padding: 10;");
+    return borderPane;
+  }
+
+  private VBox createRightVBox(){
+    VBox right = new VBox();
+    TextArea ta1 = new TextArea();
+    ta1.setStyle("-fx-max-width: 150");
+    TextArea ta2 = new TextArea();
+    ta2.setStyle("-fx-max-width: 150");
+    right.getChildren().add(ta1);
+    right.getChildren().add(ta2);
+    return right;
+  }
+
+  private HBox createTopHBox(){
+    HBox htop = new HBox();
+    htop.getChildren().add(makeButton("Button 1"));
+    htop.getChildren().add(makeButton("Button 2"));
+    htop.getChildren().add(makeButton("Button 3"));
+    return htop;
+  }
+
+  private HBox createBottomHBox(){
+    HBox bottom = new HBox();
+    TextArea ta = new TextArea();
+    ta.setStyle("-fx-max-height: 100;");
+    bottom.getChildren().add(ta);
+    return bottom;
+  }
+
+  private Button makeButton(String title){
+    return new Button(title);
   }
 
 //  private void startAnimation() {
