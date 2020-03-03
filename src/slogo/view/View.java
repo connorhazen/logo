aa80ce87eee79a5fb51e3e722ad3f611e22ead25
Sebastian Williams <svw9@duke.edu>
2020-03-02 23:20:24 -0500

    Refactored main view

--- src/slogo/view/View.java
+++ src/slogo/view/View.java
@@ -1,38 +1,23 @@
 package slogo.view;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Properties;
-import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import slogo.ControllerInterface;
 import slogo.ExceptionHelper;
 import slogo.windows.BackgroundColor;
 import slogo.windows.HelpWindow;
-import slogo.windows.ImageSelection;
 import slogo.windows.PenColorWindow;
-import slogo.windows.SelectLanguage;
 
 public class View implements ViewInterface {
 
@@ -53,22 +38,17 @@ public class View implements ViewInterface {
   private TextArea inputBox;
   private CommandHistoryView boxHistory;
 
-
-
   public View(ControllerInterface cont, Stage primaryStage, Turtle turtle){
     drawer = new TurtleDrawer();
     boxHistory = new CommandHistoryView();
-
     canvas = new Pane();
-
     errorHelper = new ExceptionHelper();
     currentTurtle = turtle;
     this.mainStage = primaryStage;
     controller = cont;
+
+    instantiateGUIElements();
     makeScreen(primaryStage);
-    errorBox = new TextArea();
-    historyBox = new TextArea();
-    inputBox = new TextArea();
     scene = new Scene(createBorderPane() , WIDTH, HEIGHT);
     scene.getStylesheets().add(STYLESHEET);
 
@@ -79,15 +59,18 @@ public class View implements ViewInterface {
 
   }
 
+  private void instantiateGUIElements(){
+    errorBox = new TextArea();
+    historyBox = new TextArea();
+    inputBox = new TextArea();
+  }
+
   private void makeKeyListens(){
     inputBox.setOnKeyPressed(event ->  {
-
         switch (event.getCode()) {
           case UP:    changeInputBox(boxHistory.getPast()); break;
-
           case DOWN:  changeInputBox(boxHistory.getNext()); break;
         }
-
     });
   }
 
@@ -101,32 +84,19 @@ public class View implements ViewInterface {
     return borderPane;
   }
 
-  private HBox createBottomHBox(){
-    //TODO: Use reflection to add run button
-    HBox bottom = new HBox();
-    bottom.getStyleClass().add("hbox-bot");
-    inputBox = new TextArea();
-    //ta.setOnKeyPressed(e -> submitText(e, ta.getText(), ta));
-    Button run = makeButton("Run", e -> {runButtonEvent();});
-    Button reset = makeButton("Reset", e -> {reset(currentTurtle);});
-    run.getStyleClass().add("run-bot");
-    reset.getStyleClass().add("run-bot");
-    bottom.getChildren().addAll(inputBox, run, reset);
-    return bottom;
-  }
-
-  private void runButtonEvent() {
-    controller.executeCommand(inputBox.getText());
-    boxHistory.add(inputBox.getText());
-    inputBox.clear();
-  }
-
   private void launchWindow(Application application){
     try {
       application.start(new Stage());
     } catch (Exception e) {
       errorHelper.fileNotFound(e);
     }
+  }
+
+  @SuppressWarnings("Used in reflection")
+  private void runButtonEvent() {
+    controller.executeCommand(inputBox.getText());
+    boxHistory.add(inputBox.getText());
+    inputBox.clear();
   }
 
   @SuppressWarnings("Used in reflection")
@@ -147,30 +117,7 @@ public class View implements ViewInterface {
     launchWindow(hw);
   }
 
-
-
-  private Button makeButton(String title, EventHandler<ActionEvent> ae){
-    Button btn = new Button(title);
-    btn.setOnAction(ae);
-    btn.setStyle("-fx-padding: 5px");
-    return btn;
-  }
-
-//  private void startAnimation() {
-//    KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step());
-//    Timeline animation = new Timeline();
-//    animation.setCycleCount(Timeline.INDEFINITE);
-//    animation.getKeyFrames().add(frame);
-//    animation.play();
-//  }
-  private void makeScreen(Stage primaryStage){
-
-  }
-
-  private void makeTurtle(){
-    drawer.addTurtleToCanvas(canvas, currentTurtle);
-  }
-
+  @SuppressWarnings("Used in reflection")
   private void reset(Turtle turtle){
     turtle.reset();
     drawer.reset();
@@ -179,6 +126,13 @@ public class View implements ViewInterface {
     drawer.addTurtleToCanvas(canvas, currentTurtle);
   }
 
+  private void makeScreen(Stage primaryStage){
+
+  }
+
+  private void makeTurtle(){
+    drawer.addTurtleToCanvas(canvas, currentTurtle);
+  }
 
   @Override
   public void printErrorFromException(Exception exception) {
