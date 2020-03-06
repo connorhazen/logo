44cff00e3c1482e0eca344bdba5f08ce17ef9e2d
Olga Suchankova <olga.suchankova@duke.edu>
2020-03-06 15:29:07 -0500

    Revert "Merge branch 'master' into 'avivah'"
    This reverts commit cbf0d7f75e80f26fe274f428a91e1c114a8c2797

--- src/slogo/Parser.java
+++ src/slogo/Parser.java
@@ -30,7 +30,7 @@ public class Parser implements ParserInterface{
     private static final Turtle DUMMY_TURTLE = new Turtle(0,0,0,0);
     public static final Class<?> NOPARAMS[] = {};
     public static final Class<?> COMMAND_CLASS_PARAMS[] = new Class<?>[] {CommandStruct.class, String.class, List.class, Turtle.class};
-    private static final Object COMMAND_PARAMS[] = new Object[] {new CommandStruct(null), "", new ArrayList<>(), DUMMY_TURTLE};
+    private static final Object COMMAND_PARAMS[] = new Object[] {new CommandStruct("", null), "", new ArrayList<>(), DUMMY_TURTLE};
     private static final Class<?> EXECUTE_CLASS_PARAMS[] = new Class<?>[] {Turtle.class};
     private static final Object EXECUTE_PARAMS[] = new Object[] {DUMMY_TURTLE};
     private static final String LIST_BEGIN_SYMBOL = "[";
--- src/slogo/commands/DoTimes.java
+++ src/slogo/commands/DoTimes.java
@@ -3,7 +3,6 @@ package slogo.commands;
 import slogo.exceptions.InvalidParameterException;
 import slogo.exceptions.UnknownCommandException;
 import slogo.structs.CommandStruct;
-import slogo.structs.VariableStruct;
 import slogo.view.Turtle;
 
 import java.util.List;
@@ -11,26 +10,10 @@ import java.util.List;
 public class DoTimes extends Misc {
     public DoTimes(CommandStruct commandStruct, String text, List<String> args, Turtle toldTurtle) {
         super(commandStruct, text, args, toldTurtle);
-        setMyNumArgs(0);
     }
 
     @Override
     protected double execute(Turtle executeOnTurtle) throws UnknownCommandException, InvalidParameterException {
-        double ret = 0;
-
-        List<Double> loopConstants = getLoopConstants();
-        double limit = loopConstants.get(0);
-        String varName = parseStringIntoVar(getListString1()).get(0);
-
-        for(double i = 1; i <= limit; i ++){
-            VariableStruct itterator = new VariableStruct(varName, i);
-            getCommandStruct().addVariable(itterator);
-            getCommandStruct().getModel().runCommand(getListString1(), executeOnTurtle);
-
-            if(i > limit ){
-                ret = retVal(getListString1());
-            }
-        }
-        return ret;
+        return 0;
     }
 }
