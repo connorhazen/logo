153aee229fc185cfa00ad9bb2fd1fa1e8a8eeeb6
Sebastian Williams <svw9@duke.edu>
2020-03-06 20:27:35 -0500

    Removed some extra strings from CommandView and put them in a properties file instead

--- src/slogo/view/ViewFactory/CommandView.java
+++ src/slogo/view/ViewFactory/CommandView.java
@@ -1,13 +1,19 @@
 package slogo.view.ViewFactory;
 
+import java.io.IOException;
+import java.util.Properties;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import slogo.ExceptionHelper;
 import slogo.view.BorderPaneLocation;
+import slogo.view.View;
 
 public class CommandView implements BorderPaneElement {
   private BorderPaneLocation loc;
+  private static final String PROPERTIES = "commandview.properties";
   public CommandView(BorderPaneLocation loc){
     this.loc = loc;
   }
@@ -16,15 +22,18 @@ public class CommandView implements BorderPaneElement {
   }
   @Override
   public Node getElement() {
+    Properties props = new Properties();
+    try{
+      props.load(View.class.getResourceAsStream(PROPERTIES));
+    } catch(IOException e){
+      new ExceptionHelper().fileNotFound(e);
+    }
     VBox left = new VBox();
     left.getStyleClass().add("vbox");
-    Label vars = new Label("Saved Variables:");
-    TextArea varBox = new TextArea();
-
-    Label coms = new Label("Saved Commands:");
-    TextArea ta2 = new TextArea();
-
-    left.getChildren().addAll(vars, varBox, coms, ta2);
+    for(String key : props.keySet().toArray(new String[0])){
+      left.getChildren().add(new Label(props.getProperty(key)));
+      left.getChildren().add(new TextArea());
+    }
     return left;
   }
 }
