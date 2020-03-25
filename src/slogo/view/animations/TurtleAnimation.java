bd05934ceeac67cfbe2431ed4400f01d8fb38655
cch57 <christopher.hazen@duke.edu>
2020-03-25 15:26:50 -0600

    commented a little more

--- src/slogo/view/animations/TurtleAnimation.java
+++ src/slogo/view/animations/TurtleAnimation.java
@@ -12,8 +12,20 @@ import javafx.util.Duration;
  */
 public interface TurtleAnimation {
 
+
+  /**
+   * This method is responsible for returning an Animation for the associated action.
+   * @param duration
+   * @return Correct Animation for turtle
+   */
   Animation getAnimation(Duration duration);
 
+
+  /**
+   * This method is used to create an instantaneous "animation" for complex loops.
+   * If we animated each step, the code would take quite long.
+   * TODO: make execute return a runnable expression to be called so it could be added to queue
+   */
   void execute();
 
 }
